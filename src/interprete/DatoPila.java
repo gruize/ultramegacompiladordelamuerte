e24182c9b0e86fc2ce2a68f9ff8d4ee5cc9aaeae
golthiryus <golthiryus@b11086b0-db55-11de-87ae-b9033455ee04>
2009-12-05 09:56:57 +0000

    Ara si lo subo :P

---
+++ src/interprete/DatoPila.java
@@ -0,0 +1,10 @@
+package interprete;
+
+/**
+ *
+ * Clase de la que heredan todos los tipos de dato de la pila. Tambien podria
+ * usarse de manera generica un Object
+ */
+public class DatoPila {
+
+}
