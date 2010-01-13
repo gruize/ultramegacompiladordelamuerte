e15aa22945121eb7cf3889e4ef533f02ce69539d
ruben.tarancon <ruben.tarancon@b11086b0-db55-11de-87ae-b9033455ee04>
2010-01-13 21:52:46 +0000

    he empezado a implementar el analizador lexico

---
+++ trunk/src/compilador/lexico/Punto_coma.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package compilador.lexico;
+
+/**
+ *
+ * @author ruben
+ */
+public class Punto_coma extends Token{
+    public Punto_coma(String l){
+        super(l);
+    }
+}
---
+++ trunk/src/compilador/lexico/Signo_menos.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package compilador.lexico;
+
+/**
+ *
+ * @author ruben
+ */
+public class Signo_menos extends Token{
+    public Signo_menos(String l){
+        super(l);
+    }
+}
