b51d66529a9e565e3920209644d49e2459b3ade3
ruben.tarancon <ruben.tarancon@b11086b0-db55-11de-87ae-b9033455ee04>
2010-01-13 22:04:33 +0000

    he empezado a implementar el analizador lexico

---
+++ src/compilador/lexico/Bool.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package compilador.lexico;
+
+/**
+ *
+ * @author ruben
+ */
+public class Bool extends Token{
+    public Bool(String l){
+        super(l);
+    }
+}
---
+++ src/compilador/lexico/Nat.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package compilador.lexico;
+
+/**
+ *
+ * @author ruben
+ */
+public class Nat extends Token{
+    public Nat(String l){
+        super(l);
+    }
+}
