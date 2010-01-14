19f128cd9ce56fd0527df6b29a4a3197f917be48
ruben.tarancon <ruben.tarancon@b11086b0-db55-11de-87ae-b9033455ee04>
2010-01-14 20:25:54 +0000

    he empezado a implementar el analizador lexico

---
+++ trunk/src/compilador/lexico/And.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package compilador.lexico;
+
+/**
+ *
+ * @author ruben
+ */
+public class And extends Token{
+    public And(String l){
+        super(l);
+    }
+}
