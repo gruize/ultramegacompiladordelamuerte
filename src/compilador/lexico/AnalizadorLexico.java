544a8ed9da2a044ed555ff05216ca41189e110f2
ruben.tarancon <ruben.tarancon@b11086b0-db55-11de-87ae-b9033455ee04>
2010-01-14 21:32:49 +0000

    Funciona pero me falta la parte de las palabras reservadas

--- src/compilador/lexico/AnalizadorLexico.java
+++ src/compilador/lexico/AnalizadorLexico.java
@@ -17,80 +17,79 @@ import java.util.ArrayList;
  * @author ruben
  */
 public class AnalizadorLexico {
-private static final int INICIAL = 0;
-private static final int PARENTESIS_C = 1;
-private static final int COMENTARIO = 2;
-private static final int PARENTESIS_A = 3;
-private static final int CASTING_INT1 =4;
-private static final int CASTING_INT2 = 5;
-private static final int CASTING_INT3 =6;
-private static final int CAST_INT = 7;
-private static final int CASTING_NAT1 = 8;
-private static final int CASTING_NAT2 = 9;
-private static final int CASTING_NAT3 = 10;
-private static final int CAST_NAT = 11;
-private static final int CASTING_FLOAT1 = 12;
-private static final int CASTING_FLOAT2 = 13;
-private static final int CASTING_FLOAT3 = 14;
-private static final int CASTING_FLOAT4 = 15;
-private static final int CASTING_FLOAT5 = 16;
-private static final int CAST_FLOAT = 17;
-private static final int CASTING_CHAR1 = 18;
-private static final int CASTING_CHAR2 = 19;
-private static final int CASTING_CHAR3 = 20;
-private static final int CASTING_CHAR4 = 21;
-private static final int CAST_CHAR = 22;
-private static final int MAYOR = 23;
-private static final int SHL = 24;
-private static final int MAYOR_IG = 25;
-private static final int MENOR = 26;
-private static final int SHR = 27;
-private static final int MENOR_IG = 28;
-private static final int DOS_PUNTOS = 29;
-private static final int DOS_PUNTOS_IG = 30;
-private static final int IGUAL = 31;
-private static final int DISTINTO1 = 32;
-private static final int DISTINTO = 33;
-private static final int LIT_NAT1 = 34;
-private static final int LIT_NAT2 = 35;
-private static final int FLOAT1 = 36;
-private static final int LIT_FLO1 = 37;
-private static final int LIT_FLO2 = 38;
-private static final int FLOAT2 = 39;
-private static final int FLOAT3 = 40;
-private static final int LIT_FLO3 = 41;
-private static final int FLOAT4 = 42;
-private static final int LIT_FLO4 = 43;
-private static final int SEPARADOR =44;
-private static final int PUNTO_COMA = 45;
-private static final int SUMA = 46;
-private static final int MULTIPLICACION = 47;
-private static final int DIVISION = 48;
-private static final int MODULO = 49;
-private static final int ABSOLUTO = 50;
-private static final int CADENA = 51;
-private static final int CHAR1 = 52;
-private static final int CHAR2 = 53;
-private static final int LIT_CHA= 54;
-private static final int SIGNO_MENOS = 55;
+    private static final int INICIAL = 0;
+    private static final int PARENTESIS_C = 1;
+    private static final int COMENTARIO = 2;
+    private static final int PARENTESIS_A = 3;
+    private static final int CASTING_INT1 =4;
+    private static final int CASTING_INT2 = 5;
+    private static final int CASTING_INT3 =6;
+    private static final int CAST_INT = 7;
+    private static final int CASTING_NAT1 = 8;
+    private static final int CASTING_NAT2 = 9;
+    private static final int CASTING_NAT3 = 10;
+    private static final int CAST_NAT = 11;
+    private static final int CASTING_FLOAT1 = 12;
+    private static final int CASTING_FLOAT2 = 13;
+    private static final int CASTING_FLOAT3 = 14;
+    private static final int CASTING_FLOAT4 = 15;
+    private static final int CASTING_FLOAT5 = 16;
+    private static final int CAST_FLOAT = 17;
+    private static final int CASTING_CHAR1 = 18;
+    private static final int CASTING_CHAR2 = 19;
+    private static final int CASTING_CHAR3 = 20;
+    private static final int CASTING_CHAR4 = 21;
+    private static final int CAST_CHAR = 22;
+    private static final int MAYOR = 23;
+    private static final int SHL = 24;
+    private static final int MAYOR_IG = 25;
+    private static final int MENOR = 26;
+    private static final int SHR = 27;
+    private static final int MENOR_IG = 28;
+    private static final int DOS_PUNTOS = 29;
+    private static final int DOS_PUNTOS_IG = 30;
+    private static final int IGUAL = 31;
+    private static final int DISTINTO1 = 32;
+    private static final int DISTINTO = 33;
+    private static final int LIT_NAT1 = 34;
+    private static final int LIT_NAT2 = 35;
+    private static final int FLOAT1 = 36;
+    private static final int LIT_FLO1 = 37;
+    private static final int LIT_FLO2 = 38;
+    private static final int FLOAT2 = 39;
+    private static final int FLOAT3 = 40;
+    private static final int LIT_FLO3 = 41;
+    private static final int FLOAT4 = 42;
+    private static final int LIT_FLO4 = 43;
+    private static final int SEPARADOR =44;
+    private static final int PUNTO_COMA = 45;
+    private static final int SUMA = 46;
+    private static final int MULTIPLICACION = 47;
+    private static final int DIVISION = 48;
+    private static final int MODULO = 49;
+    private static final int ABSOLUTO = 50;
+    private static final int CADENA = 51;
+    private static final int CHAR1 = 52;
+    private static final int CHAR2 = 53;
+    private static final int LIT_CHA= 54;
+    private static final int SIGNO_MENOS = 55;
 
-private char buff;
-private String lex;
-private int estado;
-private InputStreamReader reader = null;
-private ArrayList<Token> arrayTokens;
+    private char buff;
+    private String lex;
+    private int estado;
+    private InputStreamReader reader = null;
+    private ArrayList<Token> arrayTokens;
 
-public AnalizadorLexico(File f) throws FileNotFoundException, IOException{
-    reader = new InputStreamReader(new FileInputStream(f));
-    lex="";
-    estado = 0;
-    buff = sigCar();//inicioScan() de los apuntes
-    arrayTokens = new ArrayList<Token>();
-    scanner();
+    public AnalizadorLexico(String src) throws FileNotFoundException, IOException{
+        File f=new File(src);
+        reader = new InputStreamReader(new FileInputStream(f));
+        lex="";
+        estado = 0;
+        buff = sigCar();//inicioScan() de los apuntes
+        arrayTokens = new ArrayList<Token>();
+        scanner();
+    }
     
-
-}
-
 public char sigCar() throws IOException{
     char res;
     if(reader==null) throw new IOException("No creaste ningun Reader para la clase!");
@@ -103,8 +102,13 @@ public void transita(int est) throws IOException{
     buff = sigCar();
     estado = est;
 }
-private void error(int estado) {
+public void error(int estado) {
     }
+
+public void terminaEstado(){
+    lex="";
+    estado=INICIAL;
+}
 
 public void scanner() throws IOException{
     while(true){
@@ -146,8 +150,14 @@ public void scanner() throws IOException{
                 break;
             case PARENTESIS_C :
                 arrayTokens.add(new Parentesis_c(lex));
+                terminaEstado();
                 break;
             case COMENTARIO :
+                switch(buff){
+                    case '\n': arrayTokens.add(new Parentesis_c(lex)); terminaEstado(); break;
+                    default: transita(COMENTARIO);
+                }
+
                 break;
             case PARENTESIS_A :
                 switch (buff){
@@ -155,7 +165,7 @@ public void scanner() throws IOException{
                     case 'n': transita(CASTING_NAT1); break;
                     case 'f': transita(CASTING_FLOAT1); break;
                     case 'c': transita(CASTING_CHAR1); break;
-                    default: error(estado);
+                    default: arrayTokens.add(new Parentesis_a(lex)); terminaEstado();
                 }
                 break;
             case CASTING_INT1 :
@@ -178,6 +188,7 @@ public void scanner() throws IOException{
                 break;
             case CAST_INT :
                 arrayTokens.add(new Cast_int(lex));
+                terminaEstado();
                 break;
             case CASTING_NAT1 :
                 switch (buff){
@@ -199,6 +210,7 @@ public void scanner() throws IOException{
                 break;
             case CAST_NAT :
                 arrayTokens.add(new Cast_nat(lex));
+                terminaEstado();
                 break;
             case CASTING_FLOAT1 :
                 switch (buff){
@@ -232,6 +244,7 @@ public void scanner() throws IOException{
                 break;
             case CAST_FLOAT :
                 arrayTokens.add(new Cast_float(lex));
+                terminaEstado();
                 break;
             case CASTING_CHAR1 :
                 switch (buff){
@@ -259,46 +272,52 @@ public void scanner() throws IOException{
                 break;
             case CAST_CHAR :
                 arrayTokens.add(new Cast_char(lex));
+                terminaEstado();
                 break;
             case MAYOR :
                 switch(buff){
                     case '>': transita(SHL); break;
                     case '=': transita(MAYOR_IG); break;
-                    default: arrayTokens.add(new Mayor(lex)); break;
+                    default: arrayTokens.add(new Mayor(lex)); terminaEstado();
                 }
                 break;
             case MAYOR_IG :
                 arrayTokens.add(new Mayor_ig(lex));
+                terminaEstado();
                 break;
             case SHL :
                 arrayTokens.add(new Shl(lex));
+                terminaEstado();
                 break;
             case MENOR :
                  switch(buff){
                     case '<': transita(SHR); break;
                     case '=': transita(MENOR_IG); break;
-                    default: arrayTokens.add(new Menor(lex)); break;
+                    default: arrayTokens.add(new Menor(lex)); terminaEstado();
                 }
                 break;
             case MENOR_IG :
                 arrayTokens.add(new Menor_ig(lex));
+                terminaEstado();
                 break;
             case SHR:
                 arrayTokens.add(new Shl(lex));
+                terminaEstado();
                 break;
             case DOS_PUNTOS :
                 switch(buff){
-                    case '<': transita(DOS_PUNTOS_IG); break;
-                    default: arrayTokens.add(new Dos_puntos(lex)); break;
+                    case '=': transita(DOS_PUNTOS_IG); break;
+                    default: arrayTokens.add(new Dos_puntos(lex)); terminaEstado();
                 }
                 break;
             case DOS_PUNTOS_IG :
                 arrayTokens.add(new Dos_puntos_ig(lex));
+                terminaEstado();
                 break;
             case IGUAL:
                 switch(buff){
                     case '/': transita(DISTINTO1); break;
-                    default: error(estado);
+                    default: arrayTokens.add(new Igual(lex)); terminaEstado();
                 }
                 break;
             case DISTINTO1 :
@@ -309,17 +328,18 @@ public void scanner() throws IOException{
                 break;
             case DISTINTO:
                 arrayTokens.add(new Distinto(lex));
+                terminaEstado();
                 break;
             case LIT_NAT1 :
                 switch(buff){
                     case '.': transita(FLOAT1); break;
-                    default: arrayTokens.add(new Distinto(lex));
+                    default: arrayTokens.add(new Distinto(lex)); terminaEstado();
                 }
                 break;
             case LIT_NAT2 :
                 switch(buff){
                     case '.': transita(FLOAT1); break;
-                    default: arrayTokens.add(new Distinto(lex));
+                    default: arrayTokens.add(new Distinto(lex)); terminaEstado();
                 }
                 break;
             case FLOAT1 :
@@ -341,8 +361,10 @@ public void scanner() throws IOException{
                                 case '0': transita(FLOAT2); break;
                                 default: transita(LIT_FLO2);
                             }
-                        else
+                        else{
                             arrayTokens.add(new LitFlo(lex));
+                            terminaEstado();
+                        }
                 }
                 break;
             case LIT_FLO2 :
@@ -355,9 +377,10 @@ public void scanner() throws IOException{
                                 case '0': transita(FLOAT2); break;
                                 default: transita(LIT_FLO2);
                             }
-                        else    
+                        else{
                             arrayTokens.add(new LitFlo(lex));
-
+                            terminaEstado();
+                        }
                 }
                 break;
             case FLOAT2 :
@@ -393,33 +416,42 @@ public void scanner() throws IOException{
 
             case LIT_FLO3:
                 arrayTokens.add(new LitFlo(lex));
+                terminaEstado();
                 break;
             case LIT_FLO4 :
                 arrayTokens.add(new LitFlo(lex));
+                terminaEstado();
                 break;
             case SEPARADOR:
                 arrayTokens.add(new Separador(lex));
+                terminaEstado();
                 break;
             case PUNTO_COMA:
                 arrayTokens.add(new Punto_coma(lex));
+                terminaEstado();
                 break;
             case SUMA:
                 arrayTokens.add(new Suma(lex));
+                terminaEstado();
                 break;
             case MULTIPLICACION:
                 arrayTokens.add(new Multiplicacion(lex));
+                terminaEstado();
                 break;
             case DIVISION :
                 arrayTokens.add(new Division(lex));
+                terminaEstado();
             case MODULO:
                 arrayTokens.add(new Modulo(lex));
+                terminaEstado();
                 break;
             case ABSOLUTO:
                 arrayTokens.add(new Absoluto(lex));
+                terminaEstado();
                 break;
             case CADENA:
                 switch(buff){
-                    case 'd': transita(CADENA); break;
+                    case ' ': transita(CADENA); break;
                     default: //mirar si es palabra reservada
                 }
             case CHAR1:
@@ -435,15 +467,21 @@ public void scanner() throws IOException{
                 break;
             case LIT_CHA:
                 arrayTokens.add(new LitCha(lex));
+                terminaEstado();
                 break;
             case SIGNO_MENOS:
                 arrayTokens.add(new Signo_menos(lex));
+                terminaEstado();
                 break;
             
             
         }
     }
 }
+ public static void main(String [] args) throws FileNotFoundException, IOException {
+        AnalizadorLexico a= new AnalizadorLexico("/home/ruben/Documentos/prueba.txt");
+        a.lex="eee";
+    }
 
 }
 
