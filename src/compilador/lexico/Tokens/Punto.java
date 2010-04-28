31953e1848f3860ac079a8f1948752851aa70461
lreyero <lreyero@b11086b0-db55-11de-87ae-b9033455ee04>
2010-04-28 18:58:26 +0000

    Añado los nuevos tokens al lexico. Array, Circunflejo, Corchete_a, Corchete_c, Dispose, Do, Else, Flecha, For, Forward, Id, Llave_a, Llave_c, New, Null, Of, Pointer, Procedure, Punto, Record, Then, To, Var, While. Empiezo a actualizar el analizador lexico (analizadorER.java y AnalizadorLexico.java)
    ¡¡He comentado lo que falla para que siga compilando el proyecto!!

---
+++ src/compilador/lexico/Tokens/Punto.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package compilador.lexico.Tokens;
+
+/**
+ *
+ * @author GRUPO 3: Gonzalo Ortiz Jaureguizar, Alicia Perez Jimenez, Laura Reyero Sainz, Hector Sanjuan Redondo, Ruben Tarancon Garijo
+ */
+public class Punto extends Token{
+    public Punto(){
+        super();
+    }
+
+    @Override
+    public String toString(){
+        return "Simbolo .";
+    }
+}
